3e14908681acede3678fbd818274790d56b06300
Mattéo Legagneux <matteoo34@icloud.com>
2022-01-20 11:27:25 +0100

    inscription

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -12,48 +12,46 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConnexionController extends AbstractController
 {
-    /**
-     * @Route("/compte", name="compte")
-     */
-    public function index(): Response
-    {
-        return $this->render('connexion/index.html.twig', [
-            'controller_name' => 'ConnexionController',
-        ]);
-    }
-    /**
-     * @Route("/compte/admin/{id}", name="compte_admin")
-     */
-    public function connexionAdmin(): Response
-    {
-        return $this->render('interface_admin/index.html.twig');
-    }
-    /**
-     * @Route("/compte/customer/{id}", name="compte_customer")
-     */
-    public function connexionCustomer(): Response
-    {
-        return $this->render('interface_customer/index.html.twig');
-    }
-    /**
-     * @Route("/compte/inscription", name="inscription")
-     */
-    public function newInscription(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $customer = new Customer();
-        $form = $this->createForm(InscriptionFormType::class, $customer);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($customer);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('compte');
-        }
-        return $this->render('connexion/inscription.html.twig',[
-            'customer' => $customer,
-            'form_inscription' => $form->createView(),
-        ]);
-    }
-
+	/**
+	 * @Route("/compte", name="compte")
+	 */
+	public function index(): Response
+	{
+		return $this->render("connexion/index.html.twig", [
+			"controller_name" => "ConnexionController",
+		]);
+	}
+	/**
+	 * @Route("/compte/admin/{id}", name="compte_admin")
+	 */
+	public function connexionAdmin(): Response
+	{
+		return $this->render('interface_admin/index.html.twig');
+	}
+	/**
+	 * @Route("/compte/customer/{id}", name="compte_customer")
+	 */
+	public function connexionCustomer(): Response
+	{
+		return $this->render('interface_customer/index.html.twig');
+	}
+	/**
+	 * @Route("/compte/inscription", name="inscription")
+	 */
+	public function newInscription(Request $request, EntityManagerInterface $entityManager): Response {
+		$customer = new Customer();
+		$form = $this->createForm(InscriptionFormType::class, $customer);
+		$form->handleRequest($request);
+		if ($form->isSubmitted() && $form->isValid()) {
+			$customer->setCustomerAdmin(0);
+			$entityManager->persist($customer);
+			$entityManager->flush();
+			return $this->redirectToRoute("compte");
+		}
+		return $this->render("connexion/inscription.html.twig",[
+			"customer" => $customer,
+			"form_inscription" => $form->createView()
+		]);
+	}
 }
+?>
--- src/Form/InscriptionFormType.php
+++ src/Form/InscriptionFormType.php
@@ -1,65 +1,58 @@
 <?php
+	namespace App\Form;
+	use App\Entity\Customer;
+	use Symfony\Component\Form\Extension\Core\Type\TextType;
+	use Symfony\Component\Form\Extension\Core\Type\EmailType;
+	use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+	use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+	use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+	use Symfony\Component\Form\AbstractType;
+	use Symfony\Component\Form\FormBuilderInterface;
+	use Symfony\Component\OptionsResolver\OptionsResolver;
 
-namespace App\Form;
-
-use App\Entity\Customer;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-
-class InscriptionFormType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('customer_admin')
-            ->add('customer_first_name', TextType::class, [
-                'label' => 'Prénom',
-                'required' => true
-                ])
-            ->add('customer_last_name', TextType::class,[
-                'label' => 'Nom',
-                'required' => true
-                ])
-            ->add('customer_email', EmailType::class,[
-                'label' => 'Email',
-                'required' => true
-                ])
-            ->add('customer_phone', TextType::class,[
-                'label' => 'Numéro de téléphone',
-                'required' => false
-                ])
-            ->add('customer_password', PasswordType::class, [
-                'label' => 'Mot de passe',
-                'required' => true
-                ])
-            ->add('customer_address', TextType::class,[
-                'label' => 'Adresse',
-                'required' => true
-                ])
-            ->add('customer_post_code', IntegerType::class,[
-                'label' => 'Code Postal',
-                'required' => true
-                ])
-            ->add('customer_city', TextType::class,[
-                'label' => 'Ville',
-                'required' => true
-                ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Enregistrer'])
-    
-        ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Customer::class,
-        ]);
-    }
-}
+	class InscriptionFormType extends AbstractType {
+		public function buildForm(FormBuilderInterface $builder, array $options): void {
+			$builder
+				->add("customer_first_name", TextType::class, [
+					"label" => "Votre prénom *",
+					"required" => true
+				])
+				->add("customer_last_name", TextType::class, [
+					"label" => "Votre nom *",
+					"required" => true
+				])
+				->add("customer_email", EmailType::class, [
+					"label" => "Votre email *",
+					"required" => true
+				])
+				->add("customer_phone", TextType::class, [
+					"label" => ">Votre téléphone",
+					"required" => false
+				])
+				->add("customer_password", PasswordType::class, [
+					"label" => "Votre mot de passe *",
+					"required" => true
+				])
+				->add("customer_address", TextType::class, [
+					"label" => "Votre adresse postale *",
+					"required" => true
+				])
+				->add("customer_post_code", IntegerType::class, [
+					"label" => "Votre code postal *",
+					"required" => true
+				])
+				->add("customer_city", TextType::class, [
+					"label" => "Votre ville *",
+					"required" => true
+				])
+				->add("submit", SubmitType::class, [
+					"label" => "S'inscrire"
+				]);
+		}
+		public function configureOptions(OptionsResolver $resolver): void {
+			$resolver->setDefaults([
+				"data_class" => Customer::class
+			]);
+		}
+	}
+?>
