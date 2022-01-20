569dd60f75ca2020e209dcd9de99456e9d31699f
Clothilde <clothilde.pautis@gmail.com>
2022-01-20 12:06:54 +0100

    interface admin

--- src/Controller/InterfaceAdminController.php
+++ src/Controller/InterfaceAdminController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class InterfaceAdminController extends AbstractController
 {
     /**
-     * @Route("/interface/admin", name="interface_admin")
+     * @Route("/admin", name="interface_admin")
      */
     public function index(): Response
     {
@@ -17,4 +17,26 @@ class InterfaceAdminController extends AbstractController
             'controller_name' => 'InterfaceAdminController',
         ]);
     }
+        /**
+     * @Route("/admin/gestion-des-produits", name="manageProduct")
+     */
+    public function manageProduct(): Response
+    {
+        return $this->render('interface_admin/manage_product.html.twig');
+    }
+    /**
+     * @Route("/admin/gestion-des-clients", name="manageCustomer")
+     */
+    public function manageCustomer(): Response
+    {
+        return $this->render('interface_admin/manage_customer.html.twig');
+    }
+    /**
+     * @Route("/admin/gestion-des-commandes", name="manageOrder")
+     */
+    public function manageOrder(): Response
+    {
+        return $this->render('interface_admin/manage_order.html.twig');
+    }
+
 }
