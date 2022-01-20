cb3d325b787aa445f60429f3af3077100c9eb798
Clothilde <clothilde.pautis@gmail.com>
2022-01-20 10:03:39 +0100

    i

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
+use App\Form\InscriptionFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +21,25 @@ class ConnexionController extends AbstractController
             'controller_name' => 'ConnexionController',
         ]);
     }
+    /**
+     * @Route("/compte/inscription", name="inscription")
+     */
+    public function newInscription(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $customer = new Customer();
+        $form = $this->createForm(InscriptionFormType::class, $customer);
+        $form->handleRequest($request);
 
-    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($customer);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('compte');
+        }
+        return $this->render('connexion/inscription.html.twig',[
+            'customer' => $customer,
+            'form_inscription' => $form->createView(),
+        ]);
+    }
+
 }
--- src/Form/InscriptionFormType.php
+++ src/Form/InscriptionFormType.php
@@ -3,6 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Customer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,14 +18,41 @@ class InscriptionFormType extends AbstractType
     {
         $builder
             ->add('customer_admin')
-            ->add('customer_first_name')
-            ->add('customer_last_name')
-            ->add('customer_email')
-            ->add('customer_phone')
-            ->add('customer_password')
-            ->add('customer_address')
-            ->add('customer_post_code')
-            ->add('customer_city')
+            ->add('customer_first_name', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true
+                ])
+            ->add('customer_last_name', TextType::class,[
+                'label' => 'Nom',
+                'required' => true
+                ])
+            ->add('customer_email', EmailType::class,[
+                'label' => 'Email',
+                'required' => true
+                ])
+            ->add('customer_phone', TextType::class,[
+                'label' => 'Numéro de téléphone',
+                'required' => false
+                ])
+            ->add('customer_password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'required' => true
+                ])
+            ->add('customer_address', TextType::class,[
+                'label' => 'Adresse',
+                'required' => true
+                ])
+            ->add('customer_post_code', IntegerType::class,[
+                'label' => 'Code Postal',
+                'required' => true
+                ])
+            ->add('customer_city', TextType::class,[
+                'label' => 'Ville',
+                'required' => true
+                ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer'])
+    
         ;
     }
 
