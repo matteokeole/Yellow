f0ae435aa063eca115f8b2edf4d4134e44bec6dc
Clothilde <clothilde.pautis@gmail.com>
2022-01-21 11:41:30 +0100

    j

--- src/Controller/AdminOrderController.php
+++ src/Controller/AdminOrderController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Customer;
 use App\Entity\Order;
 use App\Form\OrderFormType;
+use App\Repository\CustomerRepository;
 use App\Repository\OrderRepository;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,62 +20,54 @@ class AdminOrderController extends AbstractController
     /**
      * @Route("/admin/gestion-des-commandes", name="manageOrder")
      */
-    public function manageOrder(OrderRepository $orderRepository): Response
+    public function manageOrder(CustomerRepository $customerRepository, OrderRepository $orderRepository): Response
     {
         return $this->render('account/manageOrder/order.html.twig', [
             'orders' => $orderRepository->findAll(),
+            'customers' => $customerRepository->findAll(),
         ]);
     }
 
-    //Ajouter un produit
-    /**
-     * @Route("/admin/gestion-des-commandes/nouveau-produit", name="manageorderNew")
-     */
-    public function neworder(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $order = new order();
-        $form = $this->createForm(orderFormType::class, $order);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($order);
-            $entityManager->flush();
-            return $this->redirectToRoute("manageorder");
-        }
-        return $this->render("account/manageorder/orderNew&Update.html.twig",[
-            "order" => $order,
-            "form_order" => $form->createView()
-        ]);
-    }
+    // /**
+    //  * @Route("/admin/gestion-des-commandes/{id}", name="manageOrderId")
+    //  */
+    // public function manageOrder(Order $order, CustomerRepository $customerRepository): Response
+    // {
+    //     return $this->render('account/manageOrder/order.html.twig', [
+    //         'order' => $order,
+    //         'customers' => $customerRepository->findBy(['order'=>$order]),
+    //     ]);
+    // }
 
-    //Afficher la fiche d'un produit
-    /**
-     * @Route("/admin/gestion-des-commandes/{id}", name="manageorderId")
-     */
-    public function manageorderId(order $order, orderRepository $orderRepository): Response
-    {
-        return $this->render('account/manageorder/orderId.html.twig', [
-            'order' => $order,
-        ]);
-    }
+    // //Afficher la fiche d'une commande
+    // /**
+    //  * @Route("/admin/gestion-des-commandes/{id}", name="manageorderId")
+    //  */
+    // public function manageorderId(order $order, orderRepository $orderRepository): Response
+    // {
+    //     return $this->render('account/manageorder/orderId.html.twig', [
+    //         'order' => $order,
+    //     ]);
+    // }
 
-    //Mise à jour d'une fiche produit
-    /**
-     * @Route("/admin/gestion-des-commandes/modifier-produit/{id}", name="manageorderUpdate")
-     */
-    public function updateorder(order $order, Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(orderFormType::class, $order);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($order);
-            $entityManager->flush();
+    // //Mise à jour d'une fiche produit
+    // /**
+    //  * @Route("/admin/gestion-des-commandes/modifier-produit/{id}", name="manageorderUpdate")
+    //  */
+    // public function updateorder(order $order, Request $request, EntityManagerInterface $entityManager): Response
+    // {
+    //     $form = $this->createForm(orderFormType::class, $order);
+    //     $form->handleRequest($request);
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager->persist($order);
+    //         $entityManager->flush();
 
-            return $this->redirectToRoute('manageorder');
-        }
+    //         return $this->redirectToRoute('manageorder');
+    //     }
 
-        return $this->render('account/manageProduct/productNew&Update.html.twig', [
-            'product' => $product,
-            'form_Product' => $form->createView(),
-        ]);
-    }
+    //     return $this->render('account/manageProduct/productNew&Update.html.twig', [
+    //         'product' => $product,
+    //         'form_Product' => $form->createView(),
+    //     ]);
+    // }
 }
