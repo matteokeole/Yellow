e50111ac04064b86fe0a384d19d8b374334683bb
Mattéo Legagneux <matteoo34@icloud.com>
2022-01-24 11:33:55 +0100

    addtobasket

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,8 +1,9 @@
 <?php
 	namespace App\Controller;
 	use App\Entity\Product;
-use App\Form\SearchFormType;
-use App\Repository\ProductRepository;
+	use App\Form\SearchFormType;
+	use App\Repository\ProductRepository;
+	use App\Session\Session;
 	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\HttpFoundation\Response;
@@ -14,13 +15,23 @@ use App\Repository\ProductRepository;
 		*/
 		public function catalog(ProductRepository $productRepository, Request $request): Response {
 			$categories = $productRepository->searchCategory();
-			$categorySearch = $request->query->get('category_search', '');
+			$categorySearch = $request->query->get("category_search", "");
 
 			return $this->render("product/catalog.html.twig", [
-				'category_search' => $categorySearch,
-				'categories' => $categories,
-				"products" => $productRepository->findAll(),
+				"category_search" => $categorySearch,
+				"categories" => $categories,
+				"products" => $productRepository->findAll()
 			]);
+		}
+		/**
+		* @Route("/ajouteraupanier/{id}", name="addtobasket")
+		*/
+		public function addToBasket(Product $product, Session $session): Response {
+			if ($session->get("customer")) {
+				// Add product to customer basket
+			}
+			// Redirect to the customer basket
+			return $this->redirectToRoute("basket");
 		}
 		/**
 		* @Route("/manga/{id}", name="product")
@@ -33,9 +44,8 @@ use App\Repository\ProductRepository;
 		/**
 		* @Route("/mangas/{category}", name="categorySearch")
 		*/
-		public function categorySearch(): Response
-		{
-				return $this->render("product/categorySearch.html.twig");
+		public function categorySearch(): Response {
+			return $this->render("product/categorySearch.html.twig");
 		}
 	}
 ?>
