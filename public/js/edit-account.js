c8e4402b9edf2e576cb232a2a74c49e480e5c38e
Mattéo Legagneux <matteoo34@icloud.com>
2022-01-25 17:40:15 +0100

    editaccount

--- public/js/edit-account.js
+++ public/js/edit-account.js
@@ -1,23 +1,133 @@
-const editAccountForm = document.querySelector("#edit-account-form"),
-	editableInputs = editAccountForm.querySelectorAll(".editable-input-container"),
-	openEditAccountForm = document.querySelector(".open-edit-account-form"),
-	cancelChanges = editAccountForm.querySelector(".cancel-changes"),
-	applyChanges = editAccountForm.querySelector(".apply-changes");
-openEditAccountForm.addEventListener("click", () => {
-	// Open edit form
-	openEditAccountForm.style.display = "none";
-	editAccountForm.lastElementChild.style.display = "flex";
-	editableInputs.forEach(e => {
-		e.lastElementChild.readOnly = false;
-		e.classList.add("editable")
+const account = {
+		form: document.querySelector("#edit-account-form"),
+		required_fields: document.querySelector(".required-fields"),
+		get inputs() {return this.form.querySelectorAll(".editable-input-container")},
+		get first_name() {return this.inputs[0].lastElementChild.value},
+		get last_name() {return this.inputs[1].lastElementChild.value},
+		get email() {return this.inputs[2].lastElementChild.value},
+		get phone() {return this.inputs[3].lastElementChild.value},
+		get password() {return this.inputs[4].lastElementChild.value},
+		get address() {return this.inputs[5].lastElementChild.value},
+		get post_code() {return this.inputs[6].lastElementChild.value},
+		get city() {return this.inputs[7].lastElementChild.value},
+		edit: document.querySelector(".open-edit-account-form"),
+		get cancel() {return this.form.querySelector(".cancel-changes")},
+		get apply() {return this.form.querySelector(".apply-changes")}
+	},
+	expr = {
+		first_name: /^[A-Za-zÀ-ú- ]+$/,
+		last_name: /^[A-Za-zÀ-ú- ]+$/,
+		email: /^[\w-\.]+@[\w-]+\.[\w-]{2,}$/,
+		phone: /^0\d{9}$/,
+		password: /^.{8,}$/,
+		address: /^[\wÀ-ú- ]+$/,
+		post_code: /^\d{5}$/,
+		city: /^[A-Za-zÀ-ú- ]+$/
+	},
+	validation = {
+		first_name: false,
+		last_name: false,
+		email: false,
+		phone: false,
+		password: false,
+		address: false,
+		post_code: false,
+		city: false
+	};
+// Edit account informations
+account.edit.addEventListener("click", () => {
+	// Hide "Edit account" button while modifying account details
+	account.edit.style.display = "none";
+	// Show required fields info
+	account.required_fields.style.display = "block";
+	// Show actions section
+	account.form.lastElementChild.style.display = "flex";
+	// Set inputs editable
+	account.inputs.forEach(input => {
+		input.lastElementChild.readOnly = false;
+		input.classList.add("editable")
 	})
 });
-cancelChanges.addEventListener("click", () => {
-	// Cancel changes & close edit form
-	openEditAccountForm.style.display = "flex";
-	editAccountForm.lastElementChild.style.display = "none";
-	editableInputs.forEach(e => {
-		e.lastElementChild.readOnly = true;
-		e.classList.remove("editable")
+// Cancel account changes
+account.cancel.addEventListener("click", () => {
+	// Show "Edit account" button
+	account.edit.style.display = "flex";
+	// Clear form error container
+	clearFormError();
+	// Hide required fields info
+	account.required_fields.style.display = "none";
+	// Hide actions section
+	account.form.lastElementChild.style.display = "none";
+	account.inputs.forEach(input => {
+		input.lastElementChild.readOnly = true;
+		input.classList.remove("editable")
 	})
+});
+// Form check-up before submitting
+account.form.addEventListener("submit", e => {
+	e.preventDefault();
+	// Clear form error container
+	clearFormError();
+	// City check-up
+	if (account.city.length > 0 && !expr.city.test(account.city)) {
+		// Invalid city
+		validation.city = false;
+		displayFormError("Champ invalide", "Veuillez renseigner un nom de ville valide.")
+	} else validation.city = true;
+	// Post code check-up
+	if (account.post_code.length > 0 && !expr.post_code.test(account.post_code)) {
+		// Invalid post code
+		validation.post_code = false;
+		displayFormError("Champ invalide", "Veuillez renseigner un code postal valide.")
+	} else validation.post_code = true;
+	// Address check-up
+	if (account.address.length > 0 && !expr.address.test(account.address)) {
+		// Invalid address
+		validation.address = false;
+		displayFormError("Champ invalide", "Veuillez renseigner une adresse postale valide.")
+	} else validation.address = true;
+	// Password check-up
+	if (account.password.length > 0 && !expr.password.test(account.password)) {
+		// Weak password
+		validation.password = false;
+		displayFormError("Mot de passe faible", "Veuillez entrer un mot de passe d'au moins 8 caractères.")
+	} else validation.password = true;
+	// Phone check-up
+	if (account.phone.length > 0 && !expr.phone.test(account.phone)) {
+		// Invalid phone
+		validation.phone = false;
+		displayFormError("Numéro de téléphone invalide", "Les espaces ne sont pas demandés.")
+	} else validation.phone = true;
+	// Email check-up
+	if (account.email.length > 0 && !expr.email.test(account.email)) {
+		// Invalid email
+		validation.email = false;
+		displayFormError("E-mail invalide", "L'e-mail que vous avez rentré est invalide.")
+	} else validation.email = true;
+	// Last name check-up
+	if (account.last_name.length > 0 && !expr.last_name.test(account.last_name)) {
+		// Invalid last name
+		validation.last_name = false;
+		displayFormError("Champ invalide", "Veuillez renseigner une valeur alphabétique dans le nom.")
+	} else validation.last_name = true;
+	// First name check-up
+	if (account.first_name.length > 0 && !expr.first_name.test(account.first_name)) {
+		// Invalid first name
+		validation.first_name = false;
+		displayFormError("Champ invalide", "Veuillez renseigner une valeur alphabétique dans le prénom.")
+	} else validation.first_name = true;
+	// Last check-up and form submit
+	if (
+		validation.first_name &&
+		validation.last_name &&
+		validation.email &&
+		validation.phone &&
+		validation.password &&
+		validation.address &&
+		validation.post_code &&
+		validation.city
+	) {
+		// Submit form
+		account.form.submit()
+	}
 })
--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -19,37 +19,72 @@
 		 */
 		public function account(OrderRepository $orderRepository, Session $session, BasketRepository $basketRepository): Response {
 			// Goto user account page if there is an active session
+			$updatedAccount = 0;
+			if ($session->get("updated-account")) {
+				$updatedAccount = $session->get("updated-account");
+				$session->clear("updated-account");
+			}
 			if ($session->get("customer")) {
 				$basket = $basketRepository->findBy(array("customer" => $session->get("customer")["id"]))[0];
 				$order = $orderRepository->findBy(array("basket" => $basket->getId()));
-
 				return $this->render("account/index.html.twig", [
 					"account" => $session->get("customer"),
 					"basket" => $basket,
-					"orders" => $order
+					"orders" => $order,
+					"update" => $updatedAccount
 				]);
 			} else return $this->redirectToRoute("login");
 		}
 		/**
 		 * @Route("/compte/modifier", name="edit-account")
 		 */
-		public function editAccount(Request $request, Customer $customer, EntityManagerInterface $entityManager, Session $session): Response {
+		public function editAccount(
+			Request $request,
+			Session $session,
+			EntityManagerInterface $entityManager,
+			CustomerRepository $customerRepository
+		): Response {
 			// Edit personal account informations
-			// $test = $request->get("customer_first_name");
-			$form = $this->createForm(CustomerFormType::class, $customer);
-			$form->handleRequest($request);
-			if ($form->isSubmitted() && $form->isValid()) {
-				$entityManager->persist($customer);
-				$entityManager->flush();
-				return $this->redirectToRoute("edit-account");
-
-			// return $this->redirectToRoute("account");
-			return $this->render("account/index.html.twig", [
-				// "test" => $test,
-				"customer" => $customer,
-				"form" => $form->createView()
+			// Get customer entity
+			$customer = $customerRepository->findBy(array("id" => $session->get("customer")["id"]))[0];
+			// Get input values
+			$first_name = $request->get("account_first_name");
+			$last_name = $request->get("account_last_name");
+			$email = $request->get("account_email");
+			$phone = $request->get("account_phone");
+			$password = $request->get("account_password");
+			$address = $request->get("account_address");
+			$post_code = $request->get("account_post_code");
+			$city = $request->get("account_city");
+			// Set input values on the customer
+			if (strlen($first_name) > 0) $customer->setCustomerFirstName($first_name);
+			if (strlen($last_name) > 0) $customer->setCustomerLastName($last_name);
+			if (strlen($email) > 0) $customer->setCustomerEmail($email);
+			$customer->setCustomerPhone($phone);
+			if (strlen($password) > 0) $customer->setCustomerPassword($password);
+			if (strlen($address) > 0) $customer->setCustomerAddress($address);
+			if (strlen($post_code) > 0) $customer->setCustomerPostCode($post_code);
+			if (strlen($city) > 0) $customer->setCustomerCity($city);
+			// Update user session
+			$session->clear("customer");
+			$session->set("customer", [
+				"id" => $customer->getId(),
+				"admin" => $customer->getCustomerAdmin(),
+				"first_name" => $customer->getCustomerFirstName(),
+				"last_name" => $customer->getCustomerLastName(),
+				"email" => $customer->getCustomerEmail(),
+				"phone" => $customer->getCustomerPhone(),
+				"password" => $customer->getCustomerPassword(),
+				"address" => $customer->getCustomerAddress(),
+				"post_code" => $customer->getCustomerPostCode(),
+				"city" => $customer->getCustomerCity()
 			]);
-			}
+			// Commit changes
+			$entityManager->flush();
+			// Display "Account updated successfully" message after the form submit
+			$session->set("updated-account", "Votre compte a été mis à jour.");
+			// Redirect to account page
+			return $this->redirectToRoute("account");
 		}
 	}
 ?>
